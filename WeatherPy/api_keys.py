3e0c462081f3cdf1d45577fbde4e158dd74bdbf6
Tucker Morris <tuckermorris@Tuckers-MBP.lan>
2024-11-11 19:56:08 -0800

    challenge 6 completed

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "5e95735ac74c76c9fe3f08b6f1fb52fb"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "034181f44e6a466d9f0aaa65e6fc2b92"
